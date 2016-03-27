11080d16e86bda20c832aae15c0ac6de3c21cfff
thinkfree1992 <857861321@qq.com>
2016-03-27 18:11:41 +0800

    add Stack in arrays

---
+++ src/chapter1/Stack.java
@@ -0,0 +1,70 @@
+package chapter1;
+
+import java.util.Iterator;
+
+public class Stack<T> implements Iterable<T>{
+	/**
+	 * define variations
+	 */
+	private T[] arr;
+	private int size;
+	
+	/**
+	 * define the size of Stack
+	 * @param capacity
+	 */
+	public Stack(int capacity){
+		arr = (T[])new Object[capacity];
+	}
+	
+	public boolean isEmpty(){
+		return size == 0;
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	public void push(T t){
+		arr[size++] = t;
+	}
+	
+	public T pop(){
+		T temp = null;
+		if(size > 0)
+		temp = arr[--size];
+		return temp;
+	}
+	
+	 public Iterator<T> iterator(){
+		return new ListIterator();
+	}
+	
+	 class ListIterator implements Iterator<T>{
+		public boolean hasNext(){
+			return size > 0;
+		}
+		
+		public void remove(){
+			
+		}
+		public T next(){
+			T temp = null;
+			while(size > 0){
+				temp = arr[--size];
+			}
+			return temp;
+		}
+	}
+	 
+	 public static void main(String[] args){
+		 Stack<String> stack = new Stack(5);
+		 stack.push("sf");
+		 stack.push("ab");
+		 stack.pop();
+		 Iterator i = stack.iterator();
+		 while(i.hasNext()){
+			 System.out.println(i.next());
+		 }
+	 }
+}
